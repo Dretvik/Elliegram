c48d50dc562a7116a62bdbc69a621dda80b31d7a
Ellie Marie <orix@hotmail.no>
2023-11-10 19:32:37 +0100

    Added some css and minor changes
    Added some pointers on the cursor to indicate that it is clickable and wil take you somewhere after login.
    Fixed the css on registration page for a more centered view.

--- pages/registrationPage/registrationPageView.js
+++ pages/registrationPage/registrationPageView.js
@@ -3,22 +3,23 @@ function registrationPageView(){
     ${logo}
         <div id="registrationPageContainer">
             <div class="registrationPageRows">
-                <span>Username:</span>
-                <input id="newUsernameInput" type="text" value="" placeholder="Enter chosen username">
+                <div class="registrationColoumnA">Username:</div>
+                <input class="registrationColoumnB" id="newUsernameInput" type="text" value="" placeholder="Enter chosen username">
             </div>
             <div class="registrationPageRows">
-            <span>Display name:</span>
-            <input id="newDisplaynameInput" type="text" value="" placeholder="Enter chosen display name">
+            <div class="registrationColoumnA">Display name:</div>
+            <input class="registrationColoumnB" id="newDisplaynameInput" type="text" value="" placeholder="Enter chosen display name">
         </div>
             <div class="registrationPageRows">
-                <span>Password:</span>
-                <input id="newPasswordInput" type="password" value="" placeholder="Enter your chosen password"> 
+                <div class="registrationColoumnA">Password:</div>
+                <input class="registrationColoumnB" id="newPasswordInput" type="password" value="" placeholder="Enter your chosen password"> 
             </div>
             <div class="registrationPageRows">
-                <span>Email adress:</span>
-                <input id="newEmailInput" type="email" value="" placeholder="Enter your email adress">
+                <div class="registrationColoumnA">Email adress:</div>
+                <input class="registrationColoumnB" id="newEmailInput" type="email" value="" placeholder="Enter your email adress">
             </div>
             <button onclick="loginPageView()">Back</button>
+            <br>
             <button onclick="registerUser()">Submit user</button>
     `;
 }
